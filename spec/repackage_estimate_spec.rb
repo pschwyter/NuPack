01e34e58c0334b3a4415d160c8eb3ae01933c6d2
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-19 00:08:40 -0500

    Refactored #new test

--- spec/repackage_estimate_spec.rb
+++ spec/repackage_estimate_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe RepackageEstimate do
 	describe "#new" do
-		before :each do
-			@repackage_estimate = RepackageEstimate.new(1,2,'food')
-		end
 
 		it "should return a RepackageEstimate object" do
-			expect(@repackage_estimate).to be_a(RepackageEstimate) 
+			repackage_estimate = RepackageEstimate.new(1,2,'food')
+			expect(repackage_estimate).to be_a(RepackageEstimate) 
 		end
+		
 	end
 
 	describe "#estimate" do
