8b664ed083674684af416e3246939144142081fe
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-18 23:00:57 -0500

    Added coding_exercise_base examples as rspec tests

--- spec/repackage_estimate_spec.rb
+++ spec/repackage_estimate_spec.rb
@@ -10,4 +10,26 @@ describe RepackageEstimate do
 			expect(@repackage_estimate).to be_a(RepackageEstimate) 
 		end
 	end
+
+	describe "#repackage_cost" do
+
+		before :each do
+			@repackage_estimate = RepackageEstimate.new('$1299.99','3 people', 'food')
+		end
+
+		it "should return the correct cost when passed a 'food' estimate" do
+			food_estimate = RepackageEstimate.new(1299.99,'3 people', 'food')
+			expect(food_estimate.repackage_cost).to eq('$1591.58')
+		end
+
+		it "should return the correct cost when passed a 'drugs' estimate" do
+			food_estimate = RepackageEstimate.new(5432,'1 person', 'drugs')
+			expect(food_estimate.repackage_cost).to eq('$6199.81')
+		end
+
+		it "should return the correct cost when passed a 'books' estimate" do
+			food_estimate = RepackageEstimate.new(12456.95,'4 people', 'books')
+			expect(food_estimate.repackage_cost).to eq('$13707.63')
+		end
+	end
 end
